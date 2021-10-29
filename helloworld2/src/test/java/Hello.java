61f74ade45a77615f92dfb7b25818fd674b1a817
ahasa <ahasa@DESKTOP-A8O5G04.station>
2021-10-29 15:43:41 +0100

    1

---
+++ helloworld2/src/test/java/Hello.java
@@ -0,0 +1,10 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub,
+		System.out.println("hi2");
+		System.out.println("Finally done with grace of allah22");
+	}
+
+}
